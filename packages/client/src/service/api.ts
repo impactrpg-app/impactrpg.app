71597b555e569906d3e57392392d38f266c681e2
Zeeshan595 <zeeshan.abid.com@gmail.com>
2025-03-18 22:11:38 +0000

    updated environment variable

--- packages/client/src/service/api.ts
+++ packages/client/src/service/api.ts
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 
-export const API_URL = import.meta.env.API_URL ?? 'http://localhost:3000';
-export const SOCKET_URL = import.meta.env.SOCKET_URL ?? 'http://localhost:3001';
+export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL ?? 'http://localhost:3001';
 export const accessToken = ref<string | null>(
     localStorage.getItem('accessToken')
 );
